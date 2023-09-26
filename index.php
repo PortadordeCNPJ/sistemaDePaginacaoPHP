2c5a2feb9dab98515fe50dba8ef8a92d9eca64c5
PortadordeCNPJ <guilhermedesouzamuller@gmail.com>
2023-09-25 21:46:32 -0300

    inicio

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../../../../favicon.ico">
+    <title>Curso de PHP devclass</title>
+
+    <!--Template based on URL below-->
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/starter-template/">
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- Place your stylesheet here-->
+    <link href="/css/stylesheet.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <h2>Curso de php</h2>
+    <div class="container">
+        <?php print_r($_GET);
+        
+        ?>
+        
+    </div>
+</body>
+</html>
